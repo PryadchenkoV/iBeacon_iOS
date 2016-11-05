e29af3ce1ce9d9406e156cac1b539f77fd7eec9a
Ivan Pryadchenko <lordpomedor@gmail.com>
2016-11-05 17:33:45 +0300

    Initial Commit

---
+++ iBeacon_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacon_iOS
+//
+//  Created by Ivan Pryadchenko on 05.11.16.
+//  Copyright © 2016 Ivan Pryadchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
